b4227b872de23000b934b05bf7e40a6d34d65bb1
tsoer18 <tsoer18@student.sdu.dk>
2023-09-06 18:04:02 +0200

    navigation bar customization

--- App.tsx
+++ App.tsx
@@ -5,13 +5,25 @@ import {NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
 import RecipeScreen from './RecipeScreen';
 
+
+
 const navigator = createStackNavigator();
+
+const HeaderBarCustomization = {
+  title: 'Let them cook',
+  
+  headerStyle: {
+    backgroundColor: '#2DB666',
+  },
+  headerTintColor: 'white',
+  elevation: 50,
+};
 
 const App = () => {
   return (
     <NavigationContainer>
       <navigator.Navigator>
-        <navigator.Screen name="RecipeScreen" component={RecipeScreen}/>
+        <navigator.Screen name="RecipeScreen" component={RecipeScreen} options={HeaderBarCustomization}/>
       </navigator.Navigator>
     </NavigationContainer>
   );
