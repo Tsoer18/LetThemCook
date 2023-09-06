b6f388246c22244cf734df8a7d109a23532f58d0
tsoer18 <tsoer18@student.sdu.dk>
2023-09-06 17:25:12 +0200

    navigation installed

---
+++ RecipeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RecipeScreen = () => {
+  return (
+    <View>
+      <Text>Hello from RecipeScreen!</Text>
+    </View>
+  );
+};
+
+export default RecipeScreen;
