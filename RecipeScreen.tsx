b6ad8d588924a1daf0b4a9d8e80cae2b81e2438c
tsoer18 <tsoer18@student.sdu.dk>
2023-09-08 09:59:15 +0200

    round button added

--- RecipeScreen.tsx
+++ RecipeScreen.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 const RecipeScreen = () => {
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.roundButton}>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create(
+  {
+    container:{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      left: 140,
+      top: 240,
+    },
+
+    roundButton:{
+      width: 90,
+      height: 90,
+      borderRadius: 100,
+      backgroundColor: '#2DB666',
+      
+      //justifyContent: 'center',
+      //alignItems: 'center',
+    },
+  }
+);
 
 export default RecipeScreen;
