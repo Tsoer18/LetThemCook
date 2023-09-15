1a22cdd2f181767d3c153f4c3b722c45b3ca02d2
tsoer18 <tsoer18@student.sdu.dk>
2023-09-15 12:58:14 +0200

    initial dropdown menu

--- AddRecipe.tsx
+++ AddRecipe.tsx
@@ -1,22 +1,113 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput} from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import { CALLBACK_TYPE } from 'react-native-gesture-handler/lib/typescript/handlers/gestures/gesture';
+import * as ImagePicker from 'react-native-image-picker';
+
+
 
 const AddRecipe = () => {
-    const [text, onChangeText] = React.useState('Useless Text');
+    
+    const [imageURI, setImageURI] = useState();
+    
+    
+    const handleImagePicker = () =>{
+        
+    /*
+        let options= {
+            Title: 'Select image',
+            MediaType: photo,
+            storageOptions: {
+                skipBackup: true
+            }
+        }
+
+
+
+        ImagePicker.launchImageLibrary(options);
+
+    // You can also use as a promise without 'callback':
+    const result = await launchImageLibrary(options);
+    */
+    }
+
+    const [recipeName, onChangeRecipeName] = React.useState('');
+    const [ingredient1, onChangeIngredient1] = React.useState('');
+    const [amount, onChangeAmount] = React.useState('');
+
+    const units = [{label: 'gram', value: 'g'}]
+
+    const [value, setValue] = React.useState('');
+
+    const handleAddIngredient = () =>{
+
+    }
+    
     return (
         <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangeText}
-                value={'Name of recipe'}/>
-            
+           
+
+            <TouchableOpacity onPress={handleImagePicker}>
+                <Text style={styles.text}>
+                    Name of Recipe
+                </Text>
+            </TouchableOpacity>
 
             <TextInput
-                style={styles.input}
-                onChangeText={onChangeText}
-                value={text}/>
+                style={styles.inputField}
+                value={recipeName}
+                placeholder='Add a recipe title'
+                placeholderTextColor='grey'
+                onChangeText={onChangeRecipeName}
+
+            />
+
+            <Text style={styles.text}>
+                Ingredients
+            </Text>
+
+            <TextInput
+                style={styles.inputFieldIngredient}
+                value={ingredient1}
+                placeholder='Add an ingredient'
+                placeholderTextColor='grey'
+                onChangeText={onChangeIngredient1}
+            />
+
+            <TextInput
+                style={styles.inputFieldIngredient}
+                value={amount}
+                placeholder='amount'
+                placeholderTextColor= 'grey'
+                onChangeText={onChangeAmount}
+            />
+
+            
+            <Dropdown 
+                labelField="label"
+                data={units}
+                search
+                onChange={ item => {setValue(item.value );}}
+                valueField="value"
+                value={value}
+            />
+
+        
+                
+            <TouchableOpacity onPress={handleAddIngredient}>
+                <Text style={styles.addFieldText}>
+                    add ingredient
+                </Text>
+            </TouchableOpacity>
+
+            <Text style={styles.text}>
+                Process
+            </Text>
         </View>
+        
     );
+   
 }
 
 const styles = StyleSheet.create({
@@ -25,9 +116,9 @@ const styles = StyleSheet.create({
       justifyContent: 'flex-start',
       alignItems: 'center',
     },
-    input: {
+    inputField: {
         height: 40,
-        width: 260,
+        width: 300,
         borderRadius: 15,
 
         margin: 12,
@@ -36,6 +127,27 @@ const styles = StyleSheet.create({
 
         color: 'black',
       },
+      inputFieldIngredient: {
+        height: 40,
+        
+        borderRadius: 15,
+
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+
+        color: 'black',
+      },
+      text: {
+        color: 'black',
+        fontSize: 18
+      },
+      addFieldText: {
+        color: 'grey',
+        fontSize: 18,
+        textDecorationLine: 'underline'
+      }
+
   });
 
 export default AddRecipe;
